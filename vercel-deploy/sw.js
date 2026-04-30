cdaf56eb8ce98a70ebf7b8f5be2cb8fd2ab6d413
cerno2611 <cerno2611@gmail.com>
2026-04-30 23:02:35 +0200

    Add files via upload

--- vercel-deploy/sw.js
+++ vercel-deploy/sw.js
@@ -1,11 +1,11 @@
-// Tento SW odregistruje sám seba a vymaže všetok cache
+// Cleanup SW - odregistruje sa a vymaže cache
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil((async () => {
     const keys = await caches.keys();
     await Promise.all(keys.map(k => caches.delete(k)));
     await self.registration.unregister();
-    const clients = await self.clients.matchAll();
+    const clients = await self.clients.matchAll({type: 'window'});
     clients.forEach(c => c.navigate(c.url));
   })());
 });
